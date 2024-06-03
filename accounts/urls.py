47ca107cef78316c8c7ded669031efef3ebc81a2
dsilentkiller <paarurawal@gmail.com>
2024-06-03 07:25:41 +0545

    added SIMPLE_JWT in settings.py and token refresh, token obtain url in accounts urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path, include
-
+from accounts import views
 urlpatterns = [
+    path('api/token/', views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+class TokenObtainPairView():
+    pass
+class TokenRefreshView():
+    pass
