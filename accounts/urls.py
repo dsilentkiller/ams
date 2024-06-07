80639991f447bd0f24566a83c4748eacf62da70c
dsilentkiller <paarurawal@gmail.com>
2024-06-07 13:48:21 +0545

    profile view and chnage passworfd sucessfully completed

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,9 +10,10 @@ urlpatterns = [
     path('send-email/', views.SendPasswordResetEmailAPIView.as_view(),
          name='send-email'),
 
-     path('profile/', views.ProfileAPIView.as_view(),
+    path('profile/', views.ProfileAPIView.as_view(),
          name='profile'),
-    path('change-password/',views.ChangePasswordAPIView.as_view(),name='change-password')
+    path('change-password/', views.ChangePasswordAPIView.as_view(),
+         name='change-password')
     # path('api/token/', views.TokenObtainPairView, name='token_obtain_pair'),
     # path('api/token/refresh/', views.TokenRefreshView, name='token_refresh'),
 ]
